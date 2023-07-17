d28d69efa3fe4e32e037e0d69157d10730ecbfc0
Eli <104906733+Bonnet2B1@users.noreply.github.com>
2023-07-17 16:55:18 +0200

    save

--- srcs/pipex.c
+++ srcs/pipex.c
@@ -6,26 +6,30 @@
 /*   By: edelarbr <edelarbr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/10 18:51:40 by edelarbr          #+#    #+#             */
-/*   Updated: 2023/07/11 18:07:55 by edelarbr         ###   ########.fr       */
+/*   Updated: 2023/07/17 16:47:35 by edelarbr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-#include <stdio.h>
+#include "pipex.h"
 
-int main(void)
+int	main(int argc, char **argv, char **env)
 {
-	pid_t	pid;
-
-	if ((pid = fork()) == -1)
-		{
-			perror("fork");
-			return 1;
-		}
-	else if (pid != 0)
-		printf("Je suis le pere, et mon pid = %d. Le pid de mon fils = %d\n", getpid(), pid);
-	else
-		printf("Je suis le fils, et mon pid = %d\n", getpid());
-	return 0;
+	t_pip	*p;
+	p = pip_init(p);
 }
- 
+
+// int main(void)
+// {
+// 	pid_t	pid;
+
+// 	if ((pid = fork()) == -1)
+// 		{
+// 			perror("fork");
+// 			return 1;
+// 		}
+// 	else if (pid != 0)
+// 		printf("Je suis le pere, et mon pid = %d. Le pid de mon fils = %d\n", getpid(), pid);
+// 	else
+// 		printf("Je suis le fils, et mon pid = %d\n", getpid());
+// 	return 0;
+// }
--- srcs/utils/struct_init.c
+++ srcs/utils/struct_init.c
@@ -1,28 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   pipex.h                                            :+:      :+:    :+:   */
+/*   struct_init.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: edelarbr <edelarbr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/11 17:55:15 by edelarbr          #+#    #+#             */
-/*   Updated: 2023/07/17 16:55:01 by edelarbr         ###   ########.fr       */
+/*   Created: 2023/07/17 16:45:25 by edelarbr          #+#    #+#             */
+/*   Updated: 2023/07/17 16:53:34 by edelarbr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PIPEX_H
-# define PIPEX_H
+#include "../pipex.h"
 
-# include <unistd.h>
-# include <stdio.h>
-# include <stdlib.h>
-
-typedef struct s_pip
-{
-	char	**argv;
-	char	**env;
-	char	*cmd;
-	int		fd;
-}			t_pip;
-
-#endif
