5b54c404379d46eba5f794d4653a189e98ce943a
Eli <104906733+Bonnet2B1@users.noreply.github.com>
2023-07-18 18:55:07 +0200

    save

--- srcs/pipex.c
+++ srcs/pipex.c
@@ -6,7 +6,7 @@
 /*   By: edelarbr <edelarbr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/10 18:51:40 by edelarbr          #+#    #+#             */
-/*   Updated: 2023/07/18 16:55:15 by edelarbr         ###   ########.fr       */
+/*   Updated: 2023/07/18 18:51:06 by edelarbr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,8 @@ int	main(int argc, char **argv, char **env)
 {
 	t_pip	*p;
 	int		pipe_fd[2];
-	int		pid;
+	int		pid1;
+	int		pid2;
 
 	(void)argc;
 	
@@ -34,19 +35,40 @@ int	main(int argc, char **argv, char **env)
 	
 	if (pipe(pipe_fd) == -1)
 		return (ft_putstr("Error: pipe\n"), 0);
-	pid = fork();
 	
-	if (pid < 0)
+	pid1 = fork();
+	if (pid1 < 0)
 		return (ft_putstr("Error: fork\n"), 0);
-	
-	else if (pid == 0)
+
+	if (pid1 == 0)
 	{
 		// chaild process (cmd1)
-		pipe_fd[1] = open(p->stdout, O_RDWR | O_CREAT | O_TRUNC, 0644);
+		dup2(open(p->stdin, O_RDONLY, 0644), STDIN_FILENO);
 		dup2(pipe_fd[1], STDOUT_FILENO);
+		close(pipe_fd[0]);
+		close(pipe_fd[1]);
 		ft_execmd(p, p->cmd1_split);
 	}
-	waitpid(pid, NULL, 0);
+	
+	pid2 = fork();
+	if (pid2 < 0)
+		return (ft_putstr("Error: fork\n"), 0);
+
+	if (pid2 == 0)
+	{
+		// chaild process (cmd2)
+		dup2(pipe_fd[0], STDIN_FILENO);
+		dup2(open(p->stdout, O_RDWR | O_CREAT | O_TRUNC, 0644) , STDOUT_FILENO);
+		close(pipe_fd[0]);
+		close(pipe_fd[1]);
+		ft_execmd(p, p->cmd2_split);
+	}
+	
+	close(pipe_fd[1]);
+	close(pipe_fd[0]);
+
+	waitpid(pid1, NULL, 0);
+	waitpid(pid2, NULL, 0);  
 
 	// system(find_cmd_path(p, "ls"));
 	// print_path(p);
